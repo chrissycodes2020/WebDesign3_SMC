b486c708c5f97bf0b70da9eba7fc65d1a70b7397
chrissycodes2020 <61220882+chrissycodes2020@users.noreply.github.com>
2021-06-08 02:34:40 -0700

    finish checkout
    css finish - validations works - start FAQ banner

--- Final Project/main.js
+++ Final Project/main.js
@@ -17,3 +17,23 @@ function placeOrder() {
      count = parseInt(items.innerHTML);
      items.innerHTML = count + parseInt(quantity.value);
  });
+
+ (function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
